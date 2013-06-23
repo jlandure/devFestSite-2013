1f0083f65d6afe19bdbcaeeca93911949d81b40b
Benjamin Petetot <bpetetot@gmail.com>
2013-06-23 15:47:28 +0200

    Correction de bug
    - Style sur mobile
    - Bug sur l'indicateur courrant de la barre de navigation quand on
    actualise la page

--- js/devfest/app.js
+++ js/devfest/app.js
@@ -7,55 +7,72 @@ angular.module('devfest', []).
       when('/sessions', {templateUrl: 'partials/sessions.html', controller: SessionsCtrl}).
       when('/speakers', {templateUrl: 'partials/speakers.html', controller: SessionsCtrl}).
       when('/agenda', {templateUrl: 'partials/agenda.html', controller: AgendaCtrl}).
-      when('/sponsors', {templateUrl: 'partials/sponsors.html', controller: SponsorsCtrl}).
+      when('/sponsors', {templateUrl: 'partials/sponsors.html', controller: EmptyCtrl}).
       when('/contacts', {templateUrl: 'partials/contacts.html', controller: EmptyCtrl}).
       when('/credits', {templateUrl: 'partials/credits.html', controller: EmptyCtrl}).
       when('/presse', {templateUrl: 'partials/presse.html', controller: EmptyCtrl}).
       otherwise({redirectTo: '/homepage'});
 }]);
 
-/* Root controller */
-RootCtrl.DEFAULT = null;
-RootCtrl.ACTIVE = 'active';
+/* Navigation controller (manage navigation bar) */
+NavigationCtrl.DEFAULT = null;
+NavigationCtrl.ACTIVE = 'active';
 
-function RootCtrl($scope) {
-  $scope.navItems = [ {'label' : 'Accueil', 'url' : '/homepage', 'style': {'class' : 'active'} }, 
+function NavigationCtrl($scope, $rootScope, $location) {
+  $scope.navItems = [ {'label' : 'Accueil', 'url' : '/homepage', 'style': {} }, 
                       {'label' : 'Sessions', 'url' : '/sessions', 'style': {} },
                       {'label' : 'Speakers', 'url' : '/speakers', 'style': {} },
                       {'label' : 'Agenda', 'url' : '/agenda', 'style': {} },
                       {'label' : 'Sponsors', 'url' : '/sponsors', 'style': {} },
                       {'label' : 'Pratique', 'url' : '/contacts', 'style': {} },
                       {'label' : 'Presse', 'url' : '/presse', 'style': {}} ];
-  $scope.selected = $scope.navItems[0];
   
-  $scope.select = function( item ) {
-    if( $scope.selected )
-        $scope.selected.style.class = RootCtrl.DEFAULT;
-    $scope.selected = item;
-    $scope.selected.style.class = RootCtrl.ACTIVE;
-  };
+  $scope.selected = getLocationItem($scope.navItems, $location.path());
 
+  $scope.select = function( item ) {
+    // Select the navigation item for the selected page
+    if( $scope.selected )
+        $scope.selected.style.class = NavigationCtrl.DEFAULT;
+    $scope.selected = item;
+    $scope.selected.style.class = NavigationCtrl.ACTIVE;
+    // Hide the sponsor div in the sponsor page
+    if (item.url == '/sponsors') {
+        $rootScope.sponsorpage = true;
+    } else {
+        $rootScope.sponsorpage = false;
+    }
+  };
+  
+  // Select the current page to the navigation bar
+  $scope.select($scope.selected );
 }
-//RootCtrl.$inject = ['$scope'];
+//NavigationCtrl.$inject = ['$scope', '$rootScope', '$location'];
+
+function getLocationItem(navItems, location) {
+  for (var i = 0; i < navItems.length; i++) {
+    if (location == navItems[i].url) {
+      return navItems[i];
+    }
+  }
+  return navItems[0];
+}
 
 /* Sessions controller */
-function SessionsCtrl($scope, $rootScope, $routeParams, $http) {
+function SessionsCtrl($scope, $routeParams, $http) {
 	$http.get('json/sessions.json').success(function(data) {
     	$scope.sessions = data.sessions;
     	$scope.speakers = data.speakers;
   	});
-   $rootScope.sponsorpage = false;
 }
 
-//SessionsCtrl.$inject = ['$scope', '$rootScope', '$routeParams' '$http'];
+//SessionsCtrl.$inject = ['$scope', '$routeParams' '$http'];
 
-function SessionDetailCtrl($scope, $rootScope) {
+function SessionDetailCtrl($scope) {
 	var speakerId = $scope.session.speaker;
 	$scope.speaker = getSpeaker($scope, speakerId);
-   $rootScope.sponsorpage = false;
 }
 
-//SessionDetailCtrl.$inject = ['$scope', '$rootScope'];
+//SessionDetailCtrl.$inject = ['$scope'];
 
 function getSpeaker(scope, speakerId) {
  	for (var i=0; i<scope.speakers.length; i++) {
@@ -66,22 +83,14 @@ function getSpeaker(scope, speakerId) {
 };
 
 /* Agenda controller */
-function AgendaCtrl($scope, $rootScope) {
-  $rootScope.sponsorpage = false;
+function AgendaCtrl($scope) {
+  
 }
-//AgendaCtrl.$inject = ['$scope', '$rootScope'];
-
-
-/* Sponsors controller */
-function SponsorsCtrl($scope, $rootScope) {
-	$rootScope.sponsorpage = true;
-}
-//SponsorsCtrl.$inject = ['$scope', '$rootScope'];
+//AgendaCtrl.$inject = ['$scope'];
 
 
 /* Empty controller */
-function EmptyCtrl($scope, $rootScope) {
+function EmptyCtrl($scope) {
   // Always nothing to do
-  $rootScope.sponsorpage = false;
 }
-//EmptyCtrl.$inject = ['$scope', '$rootScope'];
+//EmptyCtrl.$inject = ['$scope'];
