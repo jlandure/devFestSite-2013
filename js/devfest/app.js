a92532f2875548a0ba43a7e579ac577046f50a06
Benjamin Petetot <bpetetot@gmail.com>
2013-06-26 20:51:46 +0200

    Petites modifications diverses
    - Ajout d'une page afterparty
    - Désactivation des menus non dispo dans le premier déploiement
    - Ajout du lien vers le dossier de partenariat

--- js/devfest/app.js
+++ js/devfest/app.js
@@ -4,15 +4,16 @@ angular.module('devfest', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/homepage', {templateUrl: 'partials/homepage.html',   controller: EmptyCtrl}).
-      when('/sessions', {templateUrl: 'partials/sessions.html', controller: SessionsCtrl}).
+      //when('/sessions', {templateUrl: 'partials/sessions.html', controller: SessionsCtrl}).
       when('/speakers', {templateUrl: 'partials/speakers.html', controller: SessionsCtrl}).
-      when('/agenda', {templateUrl: 'partials/agenda.html', controller: AgendaCtrl}).
+      //when('/agenda', {templateUrl: 'partials/agenda.html', controller: AgendaCtrl}).
       when('/sponsors', {templateUrl: 'partials/sponsors.html', controller: EmptyCtrl}).
       when('/contacts', {templateUrl: 'partials/contacts.html', controller: EmptyCtrl}).
       when('/credits', {templateUrl: 'partials/credits.html', controller: EmptyCtrl}).
       when('/presse', {templateUrl: 'partials/presse.html', controller: EmptyCtrl}).
       when('/cfp', {templateUrl: 'partials/cfp.html', controller: EmptyCtrl}).
-       when('/subscribe', {templateUrl: 'partials/subscribe.html', controller: EmptyCtrl}).
+      //when('/subscribe', {templateUrl: 'partials/subscribe.html', controller: EmptyCtrl}).
+      //when('/afterparty', {templateUrl: 'partials/afterparty.html', controller: EmptyCtrl}).
       otherwise({redirectTo: '/homepage'});
 }]);
 
@@ -21,13 +22,18 @@ NavigationCtrl.DEFAULT = null;
 NavigationCtrl.ACTIVE = 'active';
 
 function NavigationCtrl($scope, $rootScope, $location) {
+  // Url of the sponsor document
+  $rootScope.urlPartenariat = "https://docs.google.com/file/d/0Bx5mRU2mXdx0WnBhOFVVTk90cGs/edit?usp=sharing";
+  
+  // Manage the navigation
   $scope.navItems = [ {'label' : 'Accueil', 'url' : '/homepage', 'style': {} }, 
-                      {'label' : 'Inscription', 'url' : '/subscribe', 'style': {} }, 
-                      {'label' : 'Sessions', 'url' : '/sessions', 'style': {} },
+                      //{'label' : 'Inscription', 'url' : '/subscribe', 'style': {} }, 
+                      //{'label' : 'Sessions', 'url' : '/sessions', 'style': {} },
                       {'label' : 'Speakers', 'url' : '/speakers', 'style': {} },
-                      {'label' : 'Agenda', 'url' : '/agenda', 'style': {} },
+                      //{'label' : 'Agenda', 'url' : '/agenda', 'style': {} },
                       {'label' : 'Sponsors', 'url' : '/sponsors', 'style': {} },
                       {'label' : 'Pratique', 'url' : '/contacts', 'style': {} },
+                      //{'label' : 'After Party', 'url' : '/afterparty', 'style': {} },
                       {'label' : 'Presse', 'url' : '/presse', 'style': {}} ];
   
   $scope.selected = getLocationItem($scope.navItems, $location.path());
