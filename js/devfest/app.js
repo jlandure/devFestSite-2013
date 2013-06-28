040ef0ef68e51983ba29c58b727551a8b53b7daa
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2013-06-28 22:51:42 +0200

    mise a jour des liens sociaux des speakers + refactoring du fichier app.js en vue de la validation grunt + ajout de ng-show sur les speakers pour les liens sociaux

--- js/devfest/app.js
+++ js/devfest/app.js
@@ -1,33 +1,52 @@
 
-/* ROUTE Provider */
-angular.module('devfest', []).
-  config(['$routeProvider', function($routeProvider) {
+/** 
+ * Application 
+ */
+var devfestApp = angular.module('devfest', []);
+
+
+/** 
+ *ROUTE Provider 
+ */
+devfestApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/homepage', {templateUrl: 'partials/homepage.html',   controller: EmptyCtrl}).
-      //when('/sessions', {templateUrl: 'partials/sessions.html', controller: SessionsCtrl}).
-      when('/speakers', {templateUrl: 'partials/speakers.html', controller: SessionsCtrl}).
-      //when('/agenda', {templateUrl: 'partials/agenda.html', controller: AgendaCtrl}).
-      when('/sponsors', {templateUrl: 'partials/sponsors.html', controller: EmptyCtrl}).
-      when('/contacts', {templateUrl: 'partials/contacts.html', controller: EmptyCtrl}).
-      when('/credits', {templateUrl: 'partials/credits.html', controller: EmptyCtrl}).
-      when('/presse', {templateUrl: 'partials/presse.html', controller: EmptyCtrl}).
-      when('/cfp', {templateUrl: 'partials/cfp.html', controller: EmptyCtrl}).
-      //when('/subscribe', {templateUrl: 'partials/subscribe.html', controller: EmptyCtrl}).
-      //when('/afterparty', {templateUrl: 'partials/afterparty.html', controller: EmptyCtrl}).
+      when('/homepage', {templateUrl: 'partials/homepage.html',   controller: 'EmptyCtrl'}).
+      //when('/sessions', {templateUrl: 'partials/sessions.html', controller: 'SessionsCtrl'}).
+      when('/speakers', {templateUrl: 'partials/speakers.html', controller: 'SessionsCtrl'}).
+      //when('/agenda', {templateUrl: 'partials/agenda.html', controller: 'AgendaCtrl'}).
+      when('/sponsors', {templateUrl: 'partials/sponsors.html', controller: 'EmptyCtrl'}).
+      when('/contacts', {templateUrl: 'partials/contacts.html', controller: 'EmptyCtrl'}).
+      when('/credits', {templateUrl: 'partials/credits.html', controller: 'EmptyCtrl'}).
+      when('/presse', {templateUrl: 'partials/presse.html', controller: 'EmptyCtrl'}).
+      when('/cfp', {templateUrl: 'partials/cfp.html', controller: 'EmptyCtrl'}).
+      when('/subscribe', {templateUrl: 'partials/subscribe.html', controller: 'EmptyCtrl'}).
+      //when('/afterparty', {templateUrl: 'partials/afterparty.html', controller: 'EmptyCtrl'}).
       otherwise({redirectTo: '/homepage'});
 }]);
 
-/* Navigation controller (manage navigation bar) */
-NavigationCtrl.DEFAULT = null;
-NavigationCtrl.ACTIVE = 'active';
+/** 
+ * Navigation controller (manage navigation bar) 
+ */
+devfestApp.controller('NavigationCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+  
+  var DEFAULT = null;
+  var ACTIVE = 'active';
+  
+  function getLocationItem(navItems, location) {
+    for (var i = 0; i < navItems.length; i++) {
+      if (location == navItems[i].url) {
+        return navItems[i];
+      }
+    }
+    return navItems[0];
+  }
 
-function NavigationCtrl($scope, $rootScope, $location) {
   // Url of the sponsor document
   $rootScope.urlPartenariat = "https://docs.google.com/file/d/0Bx5mRU2mXdx0WnBhOFVVTk90cGs/edit?usp=sharing";
   
   // Manage the navigation
   $scope.navItems = [ {'label' : 'Accueil', 'url' : '/homepage', 'style': {} }, 
-                      //{'label' : 'Inscription', 'url' : '/subscribe', 'style': {} }, 
+                      {'label' : 'Inscription', 'url' : '/subscribe', 'style': {} }, 
                       //{'label' : 'Sessions', 'url' : '/sessions', 'style': {} },
                       {'label' : 'Speakers', 'url' : '/speakers', 'style': {} },
                       //{'label' : 'Agenda', 'url' : '/agenda', 'style': {} },
@@ -41,9 +60,9 @@ function NavigationCtrl($scope, $rootScope, $location) {
   $scope.select = function( item ) {
     // Select the navigation item for the selected page
     if( $scope.selected )
-        $scope.selected.style.class = NavigationCtrl.DEFAULT;
+        $scope.selected.style.class = DEFAULT;
     $scope.selected = item;
-    $scope.selected.style.class = NavigationCtrl.ACTIVE;
+    $scope.selected.style.class = ACTIVE;
     // Hide the sponsor div in the sponsor page
     if (item.url == '/sponsors') {
         $rootScope.sponsorpage = true;
@@ -54,52 +73,48 @@ function NavigationCtrl($scope, $rootScope, $location) {
   
   // Select the current page to the navigation bar
   $scope.select($scope.selected );
-}
-//NavigationCtrl.$inject = ['$scope', '$rootScope', '$location'];
+}]);
 
-function getLocationItem(navItems, location) {
-  for (var i = 0; i < navItems.length; i++) {
-    if (location == navItems[i].url) {
-      return navItems[i];
-    }
-  }
-  return navItems[0];
-}
 
-/* Sessions controller */
-function SessionsCtrl($scope, $routeParams, $http) {
+/** 
+ * Sessions controller 
+ */
+devfestApp.controller('SessionsCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
 	$http.get('json/sessions.json').success(function(data) {
     	$scope.sessions = data.sessions;
     	$scope.speakers = data.speakers;
   	});
-}
+}]);
 
-//SessionsCtrl.$inject = ['$scope', '$routeParams' '$http'];
-
-function SessionDetailCtrl($scope) {
-	var speakerId = $scope.session.speaker;
-	$scope.speaker = getSpeaker($scope, speakerId);
-}
-
-//SessionDetailCtrl.$inject = ['$scope'];
-
-function getSpeaker(scope, speakerId) {
- 	for (var i=0; i<scope.speakers.length; i++) {
- 		if (scope.speakers[i].id == speakerId) {
- 			return scope.speakers[i];
- 		}
- 	}
-};
-
-/* Agenda controller */
-function AgendaCtrl($scope) {
+/** 
+ * Sessions Detail controller 
+ */
+devfestApp.controller('SessionDetailCtrl',['$scope', function ($scope) {
   
-}
-//AgendaCtrl.$inject = ['$scope'];
+  function getSpeaker(scope, speakerId) {
+   	for (var i=0; i<scope.speakers.length; i++) {
+   		if (scope.speakers[i].id == speakerId) {
+   			return scope.speakers[i];
+   		}
+   	}
+  };
+  
+  var speakerId = $scope.session.speaker;
+  $scope.speaker = getSpeaker($scope, speakerId);
 
+}]);
 
-/* Empty controller */
-function EmptyCtrl($scope) {
+/** 
+ * Agenda controller 
+ */
+devfestApp.controller('AgendaCtrl',['$scope', function ($scope) {
+  
+}]);
+
+/** 
+ * Empty controller 
+ */
+devfestApp.controller('EmptyCtrl', ['$scope', function ($scope) {
   // Always nothing to do
-}
-//EmptyCtrl.$inject = ['$scope'];
+}]);
+
